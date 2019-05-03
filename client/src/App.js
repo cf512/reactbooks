26676d62720da7f050f8b89934f1ece677604e85
Connor F <connorfinnigan@gmail.com>
2019-05-02 22:37:08 -0500

    updates to main app file

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import "./App.css";
+import Navbar from "./Components/NavBar/index"
+import { Container } from "./Components/Grid/index"
+import Jumbotron from "./Components/Jumbotron/index"
+import Home from "./pages/Home"
+import Saved from "./pages/Saved"
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Router>
+          <Navbar/>
+          <Container>
+            <Jumbotron/>
+          </Container>
+          <Switch>
+            <Route exact path="/" component = {Home}/>
+            <Route exact path="/search" component = {Home}/>
+            <Route exact path="/saved" component = {Saved}/>
+            <Route  component = {Error}/>
+          </Switch>
+      </Router>
       </div>
+      
     );
   }
 }
