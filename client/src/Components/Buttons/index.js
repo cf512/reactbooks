fde2b95a66277bff0b74e83470ff7e78a09591cc
Connor F <connorfinnigan@gmail.com>
2019-05-03 22:29:08 -0500

    new approach to components

--- client/src/Components/Buttons/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const Button = ({onClicked, children}) => {
-    return(
-        <button className="btn waves-effect waves-dark" onClicked =  {onClicked}>{children}</button>
-    )
-}
-
-export default Button;
